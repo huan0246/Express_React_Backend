27b84e07a2dda6cc6c0342041aa0cc784fa24b15
Huang Chunyen <huan0246@algonquinlive.com>
2022-05-07 20:29:18 -0400

    Modify get api route in src/routes/repos.ts for combining both data from https://api.github.com/users/silverorange/repos and provided JSON file into one array then only return repository.fork === false items

--- src/routes/repos.ts
+++ src/routes/repos.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-//import providedJSON from '../../data/repos.json';
+import providedJSON from '../../data/repos.json';
 import axios from 'axios';
 
 export const repos = Router();
@@ -9,7 +9,17 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
   axios
     .get('https://api.github.com/users/silverorange/repos')
-    .then((response) => res.send({ data: res.json(response.data) }))
+    .then((response) => {
+      const combinedArr = [...providedJSON, ...response.data];
+      const forkFalseArrInCombinedArr: any[] = [];
+      // eslint-disable-next-line array-callback-return
+      combinedArr.map((item) => {
+        if (item.fork === false) {
+          forkFalseArrInCombinedArr.push(item);
+        }
+      });
+      res.send(forkFalseArrInCombinedArr);
+    })
     .catch((err) => res.send(err));
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
