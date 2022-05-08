a2347ecca89fb6a49c520fd3cf0bad5891b15ed7
Huang Chunyen <huan0246@algonquinlive.com>
2022-05-07 20:05:31 -0400

    Install axios npm package; Fetch data from https://api.github.com/users/silverorange/repos by axios in GET method in src/routes/repos.ts

--- src/routes/repos.ts
+++ src/routes/repos.ts
@@ -1,12 +1,16 @@
 import { Router, Request, Response } from 'express';
+//import providedJSON from '../../data/repos.json';
+import axios from 'axios';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-
   res.status(200);
+  axios
+    .get('https://api.github.com/users/silverorange/repos')
+    .then((response) => res.send({ data: res.json(response.data) }))
+    .catch((err) => res.send(err));
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
 });
